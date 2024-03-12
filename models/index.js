e27a4e7e7747287173fd66d22f2a71744c1295f0
KatelynnMM <145621288+KatelynnMM@users.noreply.github.com>
2024-03-12 19:35:30 -0400

    seeds

--- models/index.js
+++ models/index.js
@@ -1,17 +1,12 @@
 const User = require('./User');
 const Recipe = require('./Recipe');
 
-// fill in associations here similar to: ///
-// User.hasMany(Post, {
-//     foreignKey: 'created_by',
-// });
+
 User.hasMany(Recipe, {
-    foreignKey: 'user_id', l
+    foreignKey: 'user_id',
 });
 
-// Post.belongsTo(User, {
-//     foreignKey: 'created_by',
-// });
+
 Recipe.belongsTo(User, {
     foreignKey: 'user_id',
 });
