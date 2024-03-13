e03057c048f9c6b355a850575d217ffea289f389
Emily Ciarabellini <136280931+ECiarabellini@users.noreply.github.com>
2024-03-12 17:35:49 -0700

    Fix Recipe and User models so server is operable

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,8 +1,16 @@
-// models/Recipe.js
-module.exports = function (sequelize, DataTypes) {
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
-    const Recipe = sequelize.define('Recipe', {
+class Recipe extends Model {}
 
+Recipe.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
         title: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -18,16 +26,19 @@ module.exports = function (sequelize, DataTypes) {
         user_id: {
             type: DataTypes.INTEGER,
             allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id',
+            }
         },
+        
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'recipe',
+    }
+);
 
-    });
-
-    Recipe.belongsTo(User, {
-        foreignKey: 'user_id',
-    });
-
-
-
-    // Return the defined Recipe model.
-    return Recipe;
-};
+module.exports = Recipe;
--- models/User.js
+++ models/User.js
@@ -1,26 +1,54 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
-const bcrypt = require('bcryptjs');
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
-module.exports = function (sequelize, DataTypes) {
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
 
-    const User = sequelize.define('User', {
-
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-    });
-
-    User.beforeCreate(async (user) => {
-
-        user.password = await bcrypt.hash(user.password, 10);
-    });
-
-
-    return User;
-};
+module.exports = User;
