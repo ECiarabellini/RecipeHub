789f01cf5acfb961055e5e660f4930cc3244f924
Emily Ciarabellini <136280931+ECiarabellini@users.noreply.github.com>
2024-03-13 23:12:40 -0700

    Add EDIT recipe route, add notes and variables to handlebars to help teammates

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -12,28 +12,6 @@ router.get('/', (req, res) => {
     res.render('login');
 });
 
-
-//one recipe detail page 
-//router.get('/recipe/:id', async (req, res) => { ///!!!use for testing in Insomnia withoutAuth
-router.get('/recipe/:id', withAuth, async (req, res) => {     //removing withAuth for testing. Add back later!
-    try {
-        const recipeID = req.params.id;
-        const recipeData = await Recipe.findByPk(recipeID);    
-        
-        if (!recipeData) {
-        return res.status(404).json('recipeData not found!');
-        }
-        const recipe = recipeData.get({ plain: true });
-        
-        // Send over the 'loggedIn' session variable to the 'recipe' template
-        res.render('recipe', { recipe, logged_in: req.session.logged_in });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
-});
-
-
 // dashboard page
 router.get('/dashboard', withAuth, async (req, res) => { 
     const userRecipes = await Recipe.findAll({
@@ -53,7 +31,43 @@ router.get('/new', withAuth, async (req, res) => {
     res.render('newRecipe', { logged_in: req.session.logged_in });
 });
 
+//one recipe detail page 
+router.get('/recipe/:id', withAuth, async (req, res) => {
+    try {
+        const recipeID = req.params.id;
+        const recipeData = await Recipe.findByPk(recipeID);    
+        
+        if (!recipeData) {
+        return res.status(404).json('recipeData not found!');
+        }
+        const recipe = recipeData.get({ plain: true });
+        console.log(recipe);
+        // Send over the 'loggedIn' session variable to the 'recipe' template
+        res.render('recipe', { recipe, logged_in: req.session.logged_in });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
+//one recipe edit page 
+router.get('/recipe/edit/:id', withAuth, async (req, res) => {
+    try {
+        const recipeID = req.params.id;
+        const recipeData = await Recipe.findByPk(recipeID);    
+        
+        if (!recipeData) {
+        return res.status(404).json('recipeData not found!');
+        }
+        const recipe = recipeData.get({ plain: true });
+        
+        // Send over the 'loggedIn' session variable to the 'recipe' template
+        res.render('editRecipe', { recipe, logged_in: req.session.logged_in });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
 
 module.exports = router;
