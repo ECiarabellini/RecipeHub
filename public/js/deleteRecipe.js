0547416a43fc202d93ec415cd768ed5556a14ac0
Emily Ciarabellini <136280931+ECiarabellini@users.noreply.github.com>
2024-03-18 11:32:13 -0700

    Fix edit recipe link

--- public/js/deleteRecipe.js
+++ public/js/deleteRecipe.js
@@ -1,10 +1,11 @@
+const recipeId = window.location.toString().split('/')[
+  window.location.toString().split('/').length - 1
+  ];
+
 async function removeRecipeHandler(event) {
     event.preventDefault();
     
     // window.location gives us access to the URL. We then use the .split() method to access the number at the end of the URL and set that equal to recipeId.
-    const recipeId = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-      ];
   
     const response = await fetch(`/api/recipe/${recipeId}`, {
       method: 'DELETE',
@@ -24,3 +25,8 @@ async function removeRecipeHandler(event) {
     .querySelector('.remove-recipe')
     .addEventListener('click', removeRecipeHandler);
   
+    
+document.getElementById("button-edit").addEventListener("click", function() {
+    window.location.href = `/recipe/edit/${recipeId}`;
+});
+
